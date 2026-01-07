1eb9947e5d6fb5e363bbfe7273670aece325f850
Hódi-Rovó Hédi <b1hodhed@vasvari.org>
2026-01-07 22:36:03 +0100

    io feladatok

--- 17 - IO feladatok/19 - Labdarugo VB/LabdarugoVB/LabdarugoVB/Program.cs
+++ 17 - IO feladatok/19 - Labdarugo VB/LabdarugoVB/LabdarugoVB/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿using LabdarugoVB;
+using System.Text;
+
+var fileData = await File.ReadAllLinesAsync("vb2018.txt", encoding: Encoding.UTF7);
+
+fileData = fileData.Skip(1).ToArray();
+
+var stadionok = new List<VB>();
+
+foreach (var line in fileData)
+{
+    var data = line.Split(';');
+
+    stadionok.Add(new VB
+    {
+        Varos = data[0],
+        Nev1 = data[1],
+        Nev2 = data[2],
+        FeroHely = int.Parse(data[3])
+    });
+}
+    Console.WriteLine($"3. feladat: Stadionok száma: {stadionok.Count}");
+
+    var legkisebbStadion = stadionok.MinBy(x => x.FeroHely);
+
+    Console.WriteLine($"4. feladat: A legkevesebb férőhely: {legkisebbStadion.ToString()}");
+
+    var atlagFerohely = stadionok.Average(x => x.FeroHely);
+
+    Console.WriteLine($"5. feladat: Átlagos férőhelyszám: {atlagFerohely}");
+
+    int stadionokSzama = 0;
+    foreach( var stadion in stadionok )
+    {
+        if (stadion.Nev2 != "n.a")
+        {
+            stadionokSzama++;
+        }
+    }
+
+    Console.WriteLine($"6. feladat: Két néven is ismert stadionok száma: {stadionokSzama}");
+
+    string szoveg = "";
+    while(szoveg.Length < 3)
+    {
+        Console.Write("Kérem a város nevét: ");
+        szoveg = Console.ReadLine();
+    }
+        bool vanE = stadionok.Any(x => x.Varos == szoveg.ToLower());
+        if( vanE )
+        {
+            Console.WriteLine("8. feladat: A megadott város VB helyszín.");
+        }
+        else
+        {
+            Console.WriteLine("8. feladat: A megadott város nem VB helyszín.");
+        }
+
+    int kulonbozoekSzama = stadionok.GroupBy(x => x.Varos).Count();
+
+    Console.WriteLine($"9. feladat: {kulonbozoekSzama} különböző városban voltak mérkőzések.");
+
+Console.ReadKey();
---
+++ 17 - IO feladatok/26 - Nobel Dijak/NobelDijak/NobelDijak/Program.cs
@@ -0,0 +1,78 @@
+﻿using NobelDijak;
+using System.Text;
+
+var fileData = await File.ReadAllLinesAsync("nobel.csv", encoding: Encoding.UTF7);
+
+fileData = fileData.Skip(1).ToArray();
+
+var tudosok = new List<Tudosok>();
+
+foreach (var line in fileData)
+{
+    var data = line.Split(';');
+
+    if(data.Length == 4)
+    {
+    tudosok.Add(new Tudosok
+    {
+        Evszam = int.Parse(data[0]),
+        Tipus = data[1],
+        Keresztnev = data[2],
+        Vezeteknev = data[3]
+    });
+    }
+
+    else
+    {
+        tudosok.Add(new Tudosok
+        {
+            Evszam = int.Parse(data[0]),
+            Tipus = data[1],
+            Keresztnev = data[2],
+            Vezeteknev = ""
+        });
+    }
+
+}
+
+var arthur = tudosok.Where(x => x.Keresztnev == "Arthur" && x.Vezeteknev == "B. McDonald").Select(x => x.Tipus);
+
+Console.WriteLine($"3. feladat: {arthur}");
+
+var irodalmi = tudosok.Where(x => x.Evszam == 2017 && x.Tipus == "irodalmi");
+
+Console.WriteLine($"4. feladat: {irodalmi.Select(x => x.Keresztnev)} {irodalmi.Select(x => x.Vezeteknev)}");
+
+List<Tudosok> szervezetek = tudosok.Where(x => x.Vezeteknev == "" && x.Evszam > 1990).ToList();
+
+Console.WriteLine("5. feladat:");
+foreach(var szervezet in szervezetek)
+{
+    Console.WriteLine($"{szervezet.Evszam}: {szervezet.Keresztnev}");
+}
+
+List<Tudosok> curiek = tudosok.Where(x => x.Keresztnev == "Curie").ToList();
+
+Console.WriteLine("6. feladat:");
+foreach (var curie in curiek)
+{
+    Console.WriteLine($"{curie.Evszam}: {curie.Keresztnev} {curie.Vezeteknev}({curie.Tipus})");
+}
+
+var tipusDarab = tudosok.GroupBy(x => x.Tipus).ToDictionary(k => k.Key, v => v.ToList().Count);
+
+Console.WriteLine("7. feladat:");
+foreach( var tipus in tipusDarab)
+{
+    Console.WriteLine($"{tipus.Key}: {tipus.Value}db");
+}
+
+var orvosok = tudosok.Where(x => x.Tipus == "orvosi").ToList();
+
+var stringBuilder = new StringBuilder();
+foreach( var orvos in orvosok)
+{
+    stringBuilder.AppendLine($"{orvos.Evszam};{orvos.Keresztnev} {orvos.Vezeteknev}");
+}
+
+await File.WriteAllTextAsync(path: "orvosi.txt", encoding: Encoding.UTF8, contents: stringBuilder.ToString());
