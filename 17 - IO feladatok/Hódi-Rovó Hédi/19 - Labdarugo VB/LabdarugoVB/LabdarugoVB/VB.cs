aff7e262a41c3e64914d457d44137d83259ab446
hedi113 <b1hodhed@vasvari.org>
2026-01-22 07:44:48 +0100

    leadando

---
+++ 17 - IO feladatok/Hódi-Rovó Hédi/19 - Labdarugo VB/LabdarugoVB/LabdarugoVB/VB.cs
@@ -0,0 +1,14 @@
+﻿namespace LabdarugoVB;
+
+public class VB
+{
+    public string Varos {  get; set; }
+    public string Nev1 { get; set; }
+    public string Nev2 { get; set; }
+    public int FeroHely { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Varos} - {Nev1}, {Nev2}, {FeroHely}";
+    }
+}
---
+++ 17 - IO feladatok/Hódi-Rovó Hédi/26 - Nobel Dijak/NobelDijak/NobelDijak/Tudosok.cs
@@ -0,0 +1,12 @@
+﻿namespace NobelDijak;
+
+public class Tudosok
+{
+    public int Evszam {  get; set; }
+
+    public string Tipus { get; set; }
+
+    public string Keresztnev { get; set; }
+
+    public string Vezeteknev { get; set; }
+}
