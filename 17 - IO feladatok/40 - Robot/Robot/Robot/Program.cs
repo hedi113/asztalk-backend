d43c552444a49e3b3a6c02a14f459b3de6ac5b55
hedi113 <b1hodhed@vasvari.org>
2026-03-04 20:14:03 +0100

    robot

--- 17 - IO feladatok/40 - Robot/Robot/Robot/Program.cs
+++ 17 - IO feladatok/40 - Robot/Robot/Robot/Program.cs
@@ -17,4 +17,60 @@ foreach (var line in file)
 //c. Annak végrehajtása során hányadik lépést követően került (légvonalban) legtávolabb a
 //robot a kiindulási ponttól és mekkora volt ez a távolság! A távolságot a lépés sorszámát követően 3 tizedes pontossággal írja a képernyőre!
 
+Console.Write("Adja meg az utasítás sorszámát: ");
 int utasitassorSzama = int.Parse(Console.ReadLine());
+
+string utasitas = utasitasok[utasitassorSzama];
+
+if((utasitas.Contains("ED") && utasitas.Contains("DE")) || (utasitas.Contains("KN") && utasitas.Contains("NK")))
+{
+    Console.WriteLine("Az utasítássor egyszerűsíthető.");
+}
+else
+{
+    Console.WriteLine("Az utasítássor nem egyszerűsíthető.");
+}
+
+GetPosition(utasitas);
+
+Dictionary<string, int> GetPosition(string utasitas)
+{
+    Dictionary<string, int> position = new();
+
+    position.Add("E", 0);
+    position.Add("D", 0);
+    position.Add("K", 0);
+    position.Add("N", 0);
+
+    foreach(char c in utasitas)
+    {
+        int value = 0;
+        if(c == char.Parse("E"))
+        {
+            position.TryGetValue("E", out value);
+            position["E"] = ++value;
+        }
+        if (c == char.Parse("D"))
+        {
+            position.TryGetValue("D", out value);
+            position["D"] = ++value;
+        }
+        if (c == char.Parse("K"))
+        {
+            position.TryGetValue("K", out value);
+            position["K"] = ++value;
+        }
+        if (c == char.Parse("N"))
+        {
+            position.TryGetValue("N", out value);
+            position["N"] = ++value;
+        }
+    }
+
+    return position;
+}
+
+int ekPositionDiff = GetPosition(utasitas)["E"] - GetPosition(utasitas)["D"];
+int knPositionDiff = GetPosition(utasitas)["K"] - GetPosition(utasitas)["N"];
+
+Console.WriteLine($"{Math.Abs(ekPositionDiff)} lépést kell tenni az ED, {Math.Abs(knPositionDiff)} lépést a KN tengely\r\nmentén.");
