e5aaf5adc5665fd45a3ed1208ab18c8094b637d3
Hódi-Rovó Hédi <b1hodhed@vasvari.org>
2026-03-15 20:05:58 +0100

    robot+lift kesz

--- 17 - IO feladatok/40 - Robot/Robot/Robot/Program.cs
+++ 17 - IO feladatok/40 - Robot/Robot/Robot/Program.cs
@@ -1,4 +1,6 @@
-﻿var file = await File.ReadAllLinesAsync("program.txt");
+﻿using System.Text;
+
+var file = await File.ReadAllLinesAsync("program.txt");
 
 List<string> utasitasok = new();
 
@@ -31,46 +33,132 @@ else
     Console.WriteLine("Az utasítássor nem egyszerűsíthető.");
 }
 
-GetPosition(utasitas);
+int x = 0, y = 0;
+double maxTav = 0;
+int lepesIndex = 0;
 
-Dictionary<string, int> GetPosition(string utasitas)
+int lepes = 0;
+
+foreach (char c in utasitas)
 {
-    Dictionary<string, int> position = new();
+    lepes++;
 
-    position.Add("E", 0);
-    position.Add("D", 0);
-    position.Add("K", 0);
-    position.Add("N", 0);
-
-    foreach(char c in utasitas)
+    switch (c)
     {
-        int value = 0;
-        if(c == char.Parse("E"))
+        case 'E': y++; break;
+        case 'D': y--; break;
+        case 'K': x++; break;
+        case 'N': x--; break;
+    }
+
+    double tav = Math.Sqrt(x * x + y * y);
+
+    if (tav > maxTav)
+    {
+        maxTav = tav;
+        lepesIndex = lepes;
+    }
+}
+
+Console.WriteLine($"Visszatérés:");
+Console.WriteLine($"{Math.Abs(y)} lépést kell tenni az ED, {Math.Abs(x)} lépést a KN tengely mentén.");
+
+Console.WriteLine($"Legtávolabbi pont: {lepesIndex} {maxTav:F3}");
+
+//3.A robot a mozgáshoz szükséges energiát egy beépített akkuból nyeri. A robot
+//1 centiméternyi távolság megtételéhez 1 egység, az irányváltásokhoz és az induláshoz
+//2 egység energiát használ. Ennek alapján az EKK utasítássor végrehajtásához 7 egység
+//energia szükséges. A szakkörön használt teljesen feltöltött kis kapacitású akkuból 100, a
+//nagykapacitásúból 1000 egységnyi energia nyerhető ki. Adja meg azon utasítássorokat,
+//amelyek végrehajtásához a teljesen feltöltött kis kapacitású akku is elegendő! Írja a képernyőre egymástól szóközzel elválasztva az utasítássor sorszámát és a szükséges energia
+//mennyiségét! Minden érintett utasítássor külön sorba kerüljön! 
+
+Console.WriteLine("\nKis akkuval végrehajtható programok:");
+
+for (int i = 0; i < utasitasok.Count; i++)
+{
+    string s = utasitasok[i];
+
+    int e = 0;
+    char elozo = ' ';
+
+    for (int j = 0; j < s.Length; j++)
+    {
+        e += 1;
+
+        if (j == 0)
+            e += 2;
+        else if (s[j] != elozo)
+            e += 2;
+
+        elozo = s[j];
+    }
+
+    if (e <= 100)
+        Console.WriteLine($"{i + 1} {e}");
+}
+
+//Gáborék továbbfejlesztették az utasításokat értelmező programot. Az új, jelenleg még
+//tesztelés alatt álló változatban a több, változatlan irányban tett elmozdulást helyettesítjük
+//az adott irányban tett elmozdulások számával és az irány betűjével. Tehát például a
+//DDDKDD utasítássor leírható rövidített 3DK2D formában is. Az önállóan álló utasításnál
+//az 1-es számot nem szabad kiírni! Hozza létre az ujprog.txt állományt, amely a
+//program.txt állományban foglalt utasítássorozatokat az új formára alakítja úgy, hogy
+//az egymást követő azonos utasításokat minden esetben a rövidített alakra cseréli! Az
+//ujprog.txt állományba soronként egy utasítássor kerüljön, a sorok ne tartalmazzanak
+//szóközt! 
+
+List<string> res = new List<string>();
+
+StringBuilder stringBuilder = new StringBuilder();
+foreach (string line in utasitasok)
+{
+    stringBuilder = new StringBuilder();
+    int count = 1;
+
+    for (int i = 1; i <= line.Length; i++)
+    {
+        if (i < line.Length && line[i] == line[i - 1])
+            count++;
+        else
         {
-            position.TryGetValue("E", out value);
-            position["E"] = ++value;
-        }
-        if (c == char.Parse("D"))
-        {
-            position.TryGetValue("D", out value);
-            position["D"] = ++value;
-        }
-        if (c == char.Parse("K"))
-        {
-            position.TryGetValue("K", out value);
-            position["K"] = ++value;
-        }
-        if (c == char.Parse("N"))
-        {
-            position.TryGetValue("N", out value);
-            position["N"] = ++value;
+            if (count > 1)
+                stringBuilder.Append(count);
+
+            stringBuilder.Append(line[i - 1]);
+            count = 1;
         }
     }
 
-    return position;
+    res.Add(stringBuilder.ToString());
 }
 
-int ekPositionDiff = GetPosition(utasitas)["E"] - GetPosition(utasitas)["D"];
-int knPositionDiff = GetPosition(utasitas)["K"] - GetPosition(utasitas)["N"];
+await File.WriteAllLinesAsync("ujprog.txt", res);
 
-Console.WriteLine($"{Math.Abs(ekPositionDiff)} lépést kell tenni az ED, {Math.Abs(knPositionDiff)} lépést a KN tengely\r\nmentén.");
+//Sajnos a tesztek rámutattak arra, hogy a program új verziója még nem tökéletes, ezért
+//vissza kell térni az utasítássorok leírásának régebbi változatához. Mivel a szakkörösök nagyon bíztak az új változatban, ezért néhány utasítássort már csak ennek megfelelően készítettek el. Segítsen ezeket visszaírni az eredeti formára! Az ismétlődések száma legfeljebb 200 lehet! Kérjen be egy új formátumú utasítássort, majd írja a képernyőre régi formában!
+
+Console.WriteLine("Adjon meg egy új formátumú utasítássort: ");
+string input = Console.ReadLine();
+
+stringBuilder = new StringBuilder();
+int number = 0;
+
+foreach (char c in input)
+{
+    if (char.IsDigit(c))
+    {
+        number = number * 10 + (c - '0');
+    }
+    else
+    {
+        int ismetles = number == 0 ? 1 : number;
+
+        for (int i = 0; i < ismetles; i++)
+            stringBuilder.Append(c);
+
+        number = 0;
+    }
+}
+
+Console.WriteLine(res.ToString());
--- 17 - IO feladatok/42 - Lift/Lift/Lift/Program.cs
+++ 17 - IO feladatok/42 - Lift/Lift/Lift/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Lift;
+using System.Collections;
 using System.Text;
 
 var fileData = await File.ReadAllLinesAsync("igeny.txt", encoding: Encoding.UTF8);
@@ -62,4 +63,106 @@ foreach (var igeny in igenyek)
     {
         utassal++;
     }
+    else
+    {
+        utasNelkul++;
+    }
 }
+
+Console.WriteLine($"Utassal: {utassal}\nUtas nélkül: {utasNelkul}");
+
+//6.Határozza meg, hogy mely szerelőcsapatok nem vették igénybe a liftet a vizsgált intervallumban! A szerelőcsapatok sorszámát egymástól egy-egy szóközzel elválasztva írja a képernyőre! 
+
+List<int> igenybeVettek = igenyek.Select(x => x.Csapatszam).Distinct().ToList();
+
+List<int> csapatszamok = new();
+for (int i = 0; i < 25; i++)
+{
+    csapatszamok.Add(i);
+}
+
+List<int> nemVettekIgenybe = igenybeVettek.Except(csapatszamok).ToList();
+Console.WriteLine("Nem vették igénybe a liftet: ");
+foreach(int i  in nemVettekIgenybe)
+{
+    Console.Write($" {i}");
+}
+
+//7.Előfordul, hogy egyik vagy másik szerelőcsapat áthágja a szabályokat, és egyik szintről
+//gyalog megy a másikra. (Ezt onnan tudhatjuk, hogy más emeleten igényli a liftet, mint
+//ahova korábban érkezett.) Generáljon véletlenszerűen egy létező csapatsorszámot! (Ha
+//nem jár sikerrel, dolgozzon a 3. csapattal!) Határozza meg, hogy a vizsgált időszak igényei alapján lehet-e egyértelműen bizonyítani, hogy ez a csapat vétett a szabályok ellen!
+//Ha igen, akkor adja meg, hogy melyik két szint közötti utat tették meg gyalog, ellenkező
+//esetben írja ki a Nem bizonyítható szabálytalanság szöveget! 
+
+Random rnd = new Random();
+int vizsgalt = rnd.Next(1, 26);
+
+bool talalt = false;
+int a = 0, b = 0;
+
+int? elozoCel = null;
+
+foreach (var i in igenyek)
+{
+    if (i.Csapatszam == vizsgalt)
+    {
+        if (elozoCel != null && elozoCel != i.InduloSzint)
+        {
+            talalt = true;
+            a = elozoCel.Value;
+            b = i.InduloSzint;
+            break;
+        }
+
+        elozoCel = i.CelSzint;
+    }
+}
+
+if (talalt)
+{
+    Console.WriteLine($"Szabálytalanság: {a} -> {b}");
+}
+else
+{
+    Console.WriteLine("Nem bizonyítható szabálytalanság");
+}
+
+//8.A munkák elvégzésének adminisztrálásához minden csapatnak egy blokkoló kártyát kell
+//használnia. A kártyára a liftben elhelyezett blokkolóóra rögzíti az emeletet, az időpontot.
+//Ennek a készüléknek a segítségével kell megadni a munka kódszámát és az adott munkafolyamat sikerességét. A munka kódja 1 és 99 közötti egész szám lehet. A sikerességet a
+//„befejezett” és a „befejezetlen” szavakkal lehet jelezni.
+//Egy műszaki hiba folytán az előző feladatban vizsgált csapat kártyájára az általunk nyomon követett időszakban nem került bejegyzés. Ezért a csapatfőnöknek a műszak végén
+//pótolnia kell a hiányzó adatokat. Az igeny.txt állomány adatait felhasználva írja a képernyőre időrendben, hogy a vizsgált időszakban milyen kérdéseket tett fel az óra, és kérje
+//be az adott válaszokat a felhasználótól! A pótlólag feljegyzett adatokat írja a blokkol.txt állományba!
+
+List<string> ki = new List<string>();
+
+foreach (var i in igenyek)
+{
+    if (i.Csapatszam == vizsgalt)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Idő: {i.IgenylesIdeje.Hours}:{i.IgenylesIdeje.Minutes}:{i.IgenylesIdeje.Seconds}");
+        Console.WriteLine($"Indulási emelet: {i.InduloSzint}");
+        Console.WriteLine($"Célemelet: {i.CelSzint}");
+
+        Console.Write("Feladatkód: ");
+        string kod = Console.ReadLine();
+
+        Console.Write("Befejezés ideje: ");
+        string ido = Console.ReadLine();
+
+        Console.Write("Sikeresség: ");
+        string siker = Console.ReadLine();
+
+        ki.Add("Indulási emelet: " + i.InduloSzint);
+        ki.Add("Célemelet: " + i.CelSzint);
+        ki.Add("Feladatkód: " + kod);
+        ki.Add("Befejezés ideje: " + ido);
+        ki.Add("Sikeresség: " + siker);
+        ki.Add("-----");
+    }
+}
+
+File.WriteAllLines("blokkol.txt", ki);
